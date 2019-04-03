93f7a00117f9ba4ba93043f4cf59d4a07784df36
ihassan1 <ihassan1@kpmg.com.au>
2019-04-03 16:15:44 +1100

    commiting inverse code

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,15 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+    inverse <- NULL
+    set <- function(y){
+        x <<- y
+        inverse <<- NULL 
+    }
+    get <- function() x
+    setInverse <- function(solveInvMatrix) inverse <<- solveInvMatrix
+    getInverse <- function() inverse
+    list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 
 }
 
@@ -12,4 +21,14 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    inverse <- x$getInverse()
+    if(!is.null(inv)){
+        message("getting cached data")
+        return(inverse)
+    }
+    data <- x$get()
+    inverse <- solve(data)
+    x$setInverse(inverse)
+    inverse
+    
 }
